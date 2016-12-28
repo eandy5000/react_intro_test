95727905994cd533fe08a3bb8ee6ba3b0f1d9024
Andy Eccleston <eandy5000@yahoo.com>
2016-12-28 10:59:12 -0600

    app and tests are all working

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
-
-export default class CommentBox extends Component {
+class CommentBox extends Component {
     constructor(props) {
         super(props)
 
@@ -12,6 +13,8 @@ export default class CommentBox extends Component {
     
     handleSubmit(event) {
         event.preventDefault()
+
+        this.props.saveComment(this.state.comment)
         this.setState({
             comment: ''
         })
@@ -35,3 +38,6 @@ export default class CommentBox extends Component {
         )
     }
 }
+
+
+export default connect(null, actions)(CommentBox)
